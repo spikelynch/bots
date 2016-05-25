693beea65c6b0a0d2c36f25f514e4f26a0b6c0af
Mike Lynch <mike.lynch@iinet.net.au>
2016-05-26 06:42:40 +1000

    Improved the comments in the TextGen library

--- amightyhost/TextGen.hs
+++ amightyhost/TextGen.hs
@@ -38,19 +38,27 @@ instance (RandomGen s) => Applicative (TextGen s) where
   pure = return
   (<*>) = ap    
 
--- Note that word returns a list of strings 
+-- Basic combinator functions
+  
+-- (word "literal") -> a TextGen which returns [ "literal" ]
+
+-- word returns [ "chars" ] so that empty results can be collapsed
+-- easily when joining the results up
 
 word :: (RandomGen g) => [Char] -> TextGen g [[Char]]
 word a = return [ a ]
 
+-- (choose [ TextGen ]) -> choose one of the TextGens in the list
                               
+-- TODO: chooseN (get multiple items from the same list without
+-- repeats)
+
 choose :: (RandomGen g) => [ TextGen g a ] -> TextGen g a
 choose options = TextGen $ \s -> let ( i, s' ) = randomR (0, (length options) - 1 ) s
                                      (TextGen optf) = options !! i
                                  in optf s'
                                    
--- needed: choose n (to get multiple items from the same list without
--- repeats
+-- (list [ TextGen ]) -> a TextGen which does every option in order
 
 list :: (RandomGen g) => [ TextGen g [ a ] ] -> TextGen g [ a ]
 list []     = TextGen $ \s -> ( [], s )
@@ -60,29 +68,36 @@ list (o:os) = TextGen $ \s -> let (TextGen ofn) = o
                                   ( as, ss' ) = osfn s'
                               in ( a ++ as, ss' )
 
---rep: Repeat a TextGen n times
+-- (rep n TextgGen) -> a TextGen which repeats n times
 
 rep :: (RandomGen g) => Int -> TextGen g [ a ] -> TextGen g [ a ]
 rep n s = list $ take n $ repeat s
 
--- randrep : Repeat a Textgen (min, max) times
+-- (randrep (min, max) TextGen) -> Repeat a Textgen (min, max) times
 
 randrep :: (RandomGen g) => (Int, Int) -> TextGen g [ a ] -> TextGen g [ a ] 
 randrep (min, max) s1 = TextGen $ \s -> let ( n, s' ) = randomR (min, max) s
                                             (TextGen s2f) = rep n s1
                                         in s2f s'
                                            
--- perhaps: 
+-- perhaps (n, m) TextGen -> Do a TextGen if rand(n) > m, otherwise empty
+
 perhaps :: (RandomGen g) => (Int, Int) -> TextGen g [ a ] -> TextGen g [ a ]
 perhaps (n, m) s1 = TextGen $ \s -> let ( n1, s' ) = randomR (0, m) s
                                         (TextGen s2f) = if n1 > n then list [] else s1
                                         in s2f s' 
+
+-- loadOptions fileName -> loads a textfile and returns a choose TextGen
 
 loadOptions :: (RandomGen g) => [Char] -> IO (TextGen g [[Char]])
 loadOptions fname = do
   contents <- fmap T.lines (Tio.readFile fname)
   return $ choose $ map ( word . T.unpack ) contents
   
+
+-- smartjoin: concatenates a list of strings with some attention to 
+-- punctuation (ie commas get stuck to their left-hand neighbours without
+-- spaces: terminal commas become full stops)
 
 smartjoin :: [ [Char] ] -> [Char]
 smartjoin ws = smartjoin_r $ undupe ws
